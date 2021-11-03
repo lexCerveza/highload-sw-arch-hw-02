973e318e8aa1cc9dc8d6385fe5b53eea6139633c
Oleksii Kraievyi <oleksiikraieviy@gmail.com>
2021-11-03 14:48:49 +0200

    fixes spinning up of elastic + run script fix

--- run.sh
+++ run.sh
@@ -6,20 +6,16 @@ sudo mkdir -p /srv/docker/elastic/data
 docker-compose up --build -d
 sudo chown -R 472:472 /srv/docker/grafana/data
 sudo chown -R 472:472 /srv/docker/mongodb/data
-sudo chown -R 472:472 /srv/docker/elastic/data
+sudo chown -R 1000:root /srv/docker/elastic/data
 
 echo "Services spinned up. Waiting for startup"
 
 sleep 10
 
-datasource=$(cat ./grafana/influxdb-datasource.json)
-curl -X POST http://admin:admin@localhost:3000/api/datasources \
- -H 'Accept: application/json' -H 'Content-Type: application/json' \
-  -d "$datasource"
+grafanaDashboard=$(<./grafana/dashboard.json)
+curl -X POST http://admin:admin@localhost:3000/api/dashboards/db -H 'Accept: application/json' -H 'Content-Type: application/json' -d "{\"dashboard\":$grafanaDashboard}"
 
-dashboard=$(cat ./grafana/dashboard.json)
-curl -X POST http://admin:admin@localhost:3000/api/dashboards/db \
- -H 'Accept: application/json' -H 'Content-Type: application/json' \
-  -d "{\"dashboard\":$dashboard"
+grafanaDatasource=$(<./grafana/influxdb-datasource.json)
+curl -X POST http://admin:admin@localhost:3000/api/datasources -H 'Accept: application/json' -H 'Content-Type: application/json' -d "$grafanaDatasource"
 
 echo "Datasource and dashboard set up"
